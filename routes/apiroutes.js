3ecf17f52a198182c79a72c7fe8f8ce0669c08b9
Dr Phil <philip.prewitt@gmail.com>
2020-09-03 22:18:04 -0600

    all apis working correctly

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,32 +1,48 @@
 const notesDB = require('../db/db.json');
 const fs = require('fs');
 const path = require('path');
+var uniqid = require('uniqid');
 
 module.exports = (app) => {
     //API req + res
-//=============================================================
+    //=============================================================
 
-//return saved notes *getNotes function
-app.get("/api/notes", function(req, res) {
-    return res.json(notesDB);
-});
+    //return saved notes *getNotes function
+    app.get("/api/notes", function (req, res) {
+        return res.json(notesDB);
+    });
 
-//create a new note in notesDB
-app.post("/api/notes", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    var newNote = req.body;
-    newNote.id = String(notesDB.length+1);
+    //create a new note in notesDB
+    app.post("/api/notes", function (req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        var newNote = req.body;
+        //assigns a unique id used to be used when deleting
+        newNote.id = uniqid()
     
-  
-    console.log(newNote);
-  
-    notesDB.push(newNote);
-  
-    res.json(newNote);
+        notesDB.push(newNote);
 
-    console.log(notesDB)
-});
+        writeFile(notesDB);
 
-app.delete("/api/notes", function(req, res) {
+        res.json(newNote);
 
+        console.log(notesDB)
+    });
+
+    app.delete("/api/notes/:id", function (req, res) {
+
+        for (let i = 0; i < notesDB.length; i++) {
+            if (notesDB[i].id === req.params.id) {
+                notesDB.splice(i, 1);
+            }
+        }
+        writeFile(notesDB);
+        res.json(notesDB)
+    })
+
+    function writeFile(notesDB) {
+        fs.writeFile('db/db.json', JSON.stringify(notesDB), function (err) {
+            if (err) throw err;
+            console.log("success");
+        });
+    }
 }
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,10 +3,12 @@ const path = require('path');
 module.exports = (app) => {
 // returning html files
 // =============================================================
+// responds with the notes page=======================
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
-
+  
+//responds with the home page========================
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
